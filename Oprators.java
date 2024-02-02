de245ff946cb6a2047916bbd51620ebcf0ea0ea9
love-sonkar <loveboss806@gmail.com>
2024-02-02 16:52:09 +0530

    practice oprators code

--- Oprators.java
+++ Oprators.java
@@ -1,6 +1,18 @@
-public class Oprators{
-    public static void main(String[] arg){
-        
-        System.out.println("Hello World");
+public class Oprators {
+    public static void main(String[] arg) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++   = " + (a++));
+        System.out.println("a--   = " + (a--));
+        System.out.println("d++   = " + (d++));
+        System.out.println("++d   = " + (++d));
     }
 }
