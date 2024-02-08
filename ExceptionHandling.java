af473ac5c30c8d5dd1a2726d6361aac9f5c83cae
love-sonkar <loveboss806@gmail.com>
2024-02-08 20:06:41 +0530

    added file reader questionn

--- ExceptionHandling.java
+++ ExceptionHandling.java
@@ -1,10 +1,18 @@
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class ExceptionHandling {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         ThrowMethods tm = new ThrowMethods();
+        // tm.tryWithResourceMethod();
         try {
-            tm.checkAge(14);
-        } catch (ArithmeticException e) {
-            System.out.println(e);
+            FileReader fr = new FileReader("test.text");
+            Scanner sc = new Scanner(fr);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
 
     }
--- ThrowMethods.java
+++ ThrowMethods.java
@@ -1,10 +1,40 @@
+import java.io.FileReader;
+
 public class ThrowMethods {
-    void checkAge(int age) throws Exception {
+    void checkAge(int age) {
         if (age < 18) {
-            throw new Exception("Access denied - You must be at least 18 years old.");
+            throw new ArithmeticException("Access denied - You must be at least 18 years old.");
         } else {
             System.out.println("Access granted - You are old enough!");
         }
+    }
+
+    // multi exception handling
+
+    void multiException() {
+        int[] arr = { 1, 3 };
+        int val = 4 / 0;
+        try {
+            // System.out.println(arr[3]);
+            System.out.println(val);
+        } catch (ArrayIndexOutOfBoundsException | ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    void tryWithResourceMethod() {
+        try {
+            FileReader fr = new FileReader("test.text");
+
+            System.out.println("hi test");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        // try (FileReader fr = new FileReader("test.text")) {
+        // fr.read();
+        // } catch (Exception e) {
+        // System.out.println(e.getMessage());
+        // }
     }
 
 }
